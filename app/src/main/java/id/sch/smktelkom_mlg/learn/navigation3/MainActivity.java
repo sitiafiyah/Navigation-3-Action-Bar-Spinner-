fbc4f317ba801f124ae213bf69be580ca42e33e0
sitiafiyah <siti_afiyah_24rpl@student.smktelkom-mlg.sch.id>
2017-02-11 19:40:42 +0700

    Action Bar Spinner Change Fragment

--- app/src/main/java/id/sch/smktelkom_mlg/learn/navigation3/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/navigation3/MainActivity.java
@@ -20,6 +20,8 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import layout.Section2Fragment;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -46,9 +48,17 @@ public class MainActivity extends AppCompatActivity {
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 // When the given dropdown item is selected, show its contents in the
                 // container view.
+
+                Fragment fragment;
+                if (position == 1) {
+                    fragment = new Section2Fragment();
+                } else {
+                    fragment = PlaceholderFragment.newInstance(position + 1);
+                }
+
                 getSupportFragmentManager().beginTransaction()
-                        .replace(R.id.container, PlaceholderFragment.newInstance(position + 1))
-                        .commit();
+                        .replace(R.id.container, fragment)
+                        .commitNow();
             }
 
             @Override
